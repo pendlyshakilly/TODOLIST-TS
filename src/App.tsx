9ea1389719c4a55e9be6de08111ff560695c0a6c
pendlyshakilly <pendlyshakilly@gmail.com>
2022-10-10 10:53:10 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <div>
+                <h3>What to learn</h3>
+                <div>
+                    <input/>
+                    <button>+</button>
+                </div>
+                <ul>
+                    <li><input type="checkbox" checked={true}/> <span>HTML</span></li>
+                    <li><input type="checkbox" checked={true}/> <span>JS</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                    <li><input type="checkbox" checked={false}/> <span>Css</span></li>
+                </ul>
+                <div>
+                    <button>All</button>
+                    <button>Active</button>
+                    <button>Completed</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
